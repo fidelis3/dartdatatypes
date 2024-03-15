1ef133fb5b18e0334fba20b0c8474baceb6e7155
bahati <bahatifidelis2004@gmail.com>
2024-03-15 08:19:53 +0300

    first commit

---
+++ datatypes.dart
@@ -0,0 +1,17 @@
+void main()
+//example of int data type 
+int age=60;
+//double data type
+double height=40.3;
+//string data type
+String name="fidelis";
+//list data type
+List<String>hobbies=["reading","Gardening","cooking"];
+//example of map data type
+Map<String, dynamic> person={"name":"alice","age":25,"city":"new york","isStudent":true};
+print("age:$age(${age.runtimeType})");
+print("height:$height(${height.runtimeType})");
+print("name:$name(${name.runtimeType})");
+print("hobbies:$hobbies(${hobbies.runtimeType})");
+print("person:$person(${person.runtimeType})");
+
